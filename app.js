6d0f57515e266876cbef577817d5e33d5c1c5581
Elisia Burt <eyburt@gmail.com>
2021-06-04 17:44:57 -0400

    We can add todos, strike them when they are done and delete them

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
 
 
 // Functions
@@ -17,7 +18,7 @@ function addTodo(event) {
     todoDiv.classList.add('todo'); 
     // Creates li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'sup brah';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Check mark button
@@ -28,13 +29,28 @@ function addTodo(event) {
     // Trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn')
+    trashButton.classList.add('trash-btn')
     todoDiv.appendChild(trashButton);
     // Append to list
     todoList.appendChild(todoDiv);
+    //clear todo input value
+    todoInput.value = ''
 
 
+}
 
+function deleteCheck(e){
+    const item = e.target;
+    //delete todo
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.remove()
+    }
 
-
+    //check mark
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+        console.log('clicked!!!')
+    }
 }
